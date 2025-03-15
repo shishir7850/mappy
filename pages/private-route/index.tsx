769758761733b5a133c050667b2c14d47e091c11
shishir7850 <shishir.lakkadi@gmail.com>
2025-03-15 20:26:14 +0000

    Set up Next.js with Clerk, Prisma, and copied code

---
+++ pages/private-route/index.tsx
@@ -0,0 +1,9 @@
+const setupIndex = (any: any) => {
+    return(
+        <h1>
+            Hello World
+        </h1>
+    )
+}
+
+export default setupIndex;
---
+++ pages/public-route/index.tsx
@@ -0,0 +1,9 @@
+const publicRoute = (any: any) => {
+    return(
+        <h1>
+            This is a public Route.
+        </h1>
+    )
+}
+
+export default publicRoute;
